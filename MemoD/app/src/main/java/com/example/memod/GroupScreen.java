6943c88dfb0d33f1bdbeb3d1787853794a1bfd13
thisisrealjh <atsalamall@gmail.com>
2021-08-30 23:20:23 +0900

    xml 파일명 수정

--- MemoD/app/src/main/java/com/example/memod/GroupScreen.java
+++ MemoD/app/src/main/java/com/example/memod/GroupScreen.java
@@ -9,6 +9,6 @@ public class GroupScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_group_screen);
+        setContentView(R.layout.activity_groupmain);
     }
 }
