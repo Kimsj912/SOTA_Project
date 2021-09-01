f8c80b99c1d36fa29bdd5136361fa076f7b1213e
portfolio_1st <aa991122aa@gmail.com>
2021-09-01 16:16:03 +0900

    temp commit

--- MemoD/app/src/main/java/com/example/memod/EditProfileActivity.java
+++ MemoD/app/src/main/java/com/example/memod/EditProfileActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -11,11 +12,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class EditProfileActivity extends AppCompatActivity {
 
+    private EditText  self_intro, user_id, name, pw, ph,birth_year,sex, location1,location2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+
+        self_intro = findViewById(R.id.self_intro);
+        user_id = findViewById(R.id.user_id);
+        name = findViewById(R.id.name);
+        pw = findViewById(R.id.pw);
+        ph = findViewById(R.id.ph);
+        birth_year = findViewById(R.id.birth_year);
+        sex = findViewById(R.id.sex);
+        location1 = findViewById(R.id.location1);
+        location2 = findViewById(R.id.location2);
+
+
+
+
+
+
 
         ImageView close = (ImageView) findViewById(R.id.close);
         close.setOnClickListener(new View.OnClickListener() {
