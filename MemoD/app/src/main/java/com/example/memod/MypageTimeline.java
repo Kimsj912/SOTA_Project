cef8c117d98df687a1e213c654ae3ee413259c44
portfolio_1st <aa991122aa@gmail.com>
2021-09-01 15:50:25 +0900

    MypageTimeline 테스트값 넣기 완료, mypage_map.xml 핀 찍기 완료

--- MemoD/app/src/main/java/com/example/memod/MypageTimeline.java
+++ MemoD/app/src/main/java/com/example/memod/MypageTimeline.java
@@ -3,10 +3,17 @@ package com.example.memod;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
 
 public class MypageTimeline extends AppCompatActivity {
 
@@ -50,4 +57,30 @@ public class MypageTimeline extends AppCompatActivity {
             }
         });
 
-}}
+        // set List ---->
+        // <test Data>
+        ArrayList<ListGeneral> dataList = new ArrayList<ListGeneral>();
+        dataList.add(new ListTimeLineRow("2021년 8월 22일","여의도 한강공원",null));
+        dataList.add(new ListTimeLineRow("2021년 8월 25일","명지대학교 인문캠퍼스",null));
+        dataList.add(new ListTimeLineRow("2021년 8월 29일","AK& 홍대",null));
+        dataList.add(new ListTimeLineRow("2021년 8월 31일","스타벅스 강남삼성타운점",null));
+        // insert data into listview
+        ListView listView = (ListView) findViewById(R.id.ListView);
+        final MyListAdapter myListAdapter = new MyListAdapter(this,dataList){
+            @Override
+            public View getView(int position, View converView, ViewGroup parent) {
+                View view = mLayoutInflater.inflate(R.layout.timeline_row, null,false);
+
+                TextView date = (TextView)view.findViewById(R.id.date);
+                TextView place = (TextView)view.findViewById(R.id.place);
+
+                date.setText(((ListTimeLineRow)sample.get(position)).getDate());
+                place.setText(((ListTimeLineRow)sample.get(position)).getPlace());
+
+                return view;
+            }
+        };
+
+        listView.setAdapter(myListAdapter) ;
+    }
+}
