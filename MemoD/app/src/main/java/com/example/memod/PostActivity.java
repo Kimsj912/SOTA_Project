4741aa3827c758fc4c93a2594a8038170908e592
portfolio_1st <aa991122aa@gmail.com>
2021-09-01 20:57:36 +0900

    Postview 연결 안되는 문제 해결

--- MemoD/app/src/main/java/com/example/memod/PostActivity.java
+++ MemoD/app/src/main/java/com/example/memod/PostActivity.java
@@ -14,16 +14,6 @@ public class PostActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
 
-        ImageView imageView8 = (ImageView) findViewById(R.id.imageView8);
-        imageView8.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(PostActivity.this , GroupScreen.class);
-                startActivity(intent);
-            }
-        });
-
-
 
         ImageView home = (ImageView) findViewById(R.id.home);
         home.setOnClickListener(new View.OnClickListener() {
